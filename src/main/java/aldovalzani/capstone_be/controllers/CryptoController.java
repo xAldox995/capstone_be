0a6da7f0698cacffd797903e0c06a11d4708e24d
xAldox995 <valzanialdo@gmail.com>
2024-11-21 23:06:42 +0100

    i need to test for understand

---
+++ src/main/java/aldovalzani/capstone_be/controllers/CryptoController.java
@@ -0,0 +1,7 @@
+package aldovalzani.capstone_be.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CryptoController {
+}
--- src/main/java/aldovalzani/capstone_be/tools/CryptoCompareServ.java
+++ src/main/java/aldovalzani/capstone_be/tools/CryptoCompareServ.java
@@ -1,4 +1,21 @@
 package aldovalzani.capstone_be.tools;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@Service
 public class CryptoCompareServ {
+    private RestTemplate restTemplate;
+    @Value("${fetch.url}")
+    private static String fetchUrlBase;//->corrisponde a questo URL per ora https://min-api.cryptocompare.com/data/price
+    @Value("${api}")
+    private static String apiKey;
+
+    public Map<String, Double>  getCryptoPrice (String symbol, String currency){
+       String fullUrl = fetchUrlBase+"?fsym="+symbol+"&tsyms="+currency+"&api_key="+apiKey;
+       return restTemplate.getForObject(fullUrl, Map.class);
+    }
 }
