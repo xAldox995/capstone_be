ba2771aac8fab5052cd3e5d7674cfb1f5d776561
xAldox995 <valzanialdo@gmail.com>
2024-11-22 09:38:06 +0100

    OK now the call to api go

--- src/main/java/aldovalzani/capstone_be/controllers/CryptoController.java
+++ src/main/java/aldovalzani/capstone_be/controllers/CryptoController.java
@@ -1,7 +1,27 @@
 package aldovalzani.capstone_be.controllers;
 
+import aldovalzani.capstone_be.services.CryptoCompareServ;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
+/*
+http://localhost:3001/api
+ */
 @RestController
+@RequestMapping("/api")
 public class CryptoController {
+    @Autowired
+    private CryptoCompareServ cryptoCompareServ;
+
+    @GetMapping ("/crypto/price")
+    public Map<String,Double> fetchCryptoPrice(
+            @RequestParam String symbol,
+            @RequestParam String currency){
+        return cryptoCompareServ.getCryptoPrice(symbol,currency);
+    }
 }
--- src/main/java/aldovalzani/capstone_be/services/CryptoCompareServ.java
+++ src/main/java/aldovalzani/capstone_be/services/CryptoCompareServ.java
@@ -1,5 +1,6 @@
-package aldovalzani.capstone_be.tools;
+package aldovalzani.capstone_be.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -8,11 +9,12 @@ import java.util.Map;
 
 @Service
 public class CryptoCompareServ {
+    @Autowired
     private RestTemplate restTemplate;
     @Value("${fetch.url}")
-    private static String fetchUrlBase;//->corrisponde a questo URL per ora https://min-api.cryptocompare.com/data/price
-    @Value("${api}")
-    private static String apiKey;
+    private  String fetchUrlBase;//->corrisponde a questo URL per ora https://min-api.cryptocompare.com/data/price
+    @Value("${api.key}")
+    private  String apiKey;
 
     public Map<String, Double>  getCryptoPrice (String symbol, String currency){
        String fullUrl = fetchUrlBase+"?fsym="+symbol+"&tsyms="+currency+"&api_key="+apiKey;
