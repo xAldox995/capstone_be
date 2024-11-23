208bceda7069c5e06c2e78ae6fe8b6028cb4bec5
xAldox995 <valzanialdo@gmail.com>
2024-11-24 00:44:43 +0100

    some annotation and now i need to test

--- src/main/java/aldovalzani/capstone_be/security/SecurityConfig.java
+++ src/main/java/aldovalzani/capstone_be/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package aldovalzani.capstone_be.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
