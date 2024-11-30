f8eae2afb10751cb6720c7ba714b66e1302aa3de
xAldox995 <valzanialdo@gmail.com>
2024-11-30 21:21:27 +0100

    some fix for dto transazione

--- src/main/java/aldovalzani/capstone_be/dto/TransazioneDTO.java
+++ src/main/java/aldovalzani/capstone_be/dto/TransazioneDTO.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotNull;
 public record TransazioneDTO(
         @NotNull long idWallet,
         @NotNull long idCrypto,
-        @NotNull long quantita,
-        @NotNull long prezzo,
+        @NotNull double quantita,
+        @NotNull double prezzo,
         @NotBlank String tipoTransazione
 ) {}
