f48c0004d78f532a05cb4f3e6a76298faeea707e
xAldox995 <valzanialdo@gmail.com>
2024-12-08 23:26:15 +0100

    adds bean for cors and it works

--- src/main/java/aldovalzani/capstone_be/security/SecurityConfig.java
+++ src/main/java/aldovalzani/capstone_be/security/SecurityConfig.java
@@ -12,6 +12,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -42,5 +48,19 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder getCrypt(){
         return new BCryptPasswordEncoder(12);
+    }
+
+    //http://localhost:5173/
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
